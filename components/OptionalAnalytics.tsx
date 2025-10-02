40d6b3bd12acdd052852499a82560305793ceebe
Matt Kneale <anonymort@protonmail.com>
2025-10-02 12:23:31 +0100

    Enhance OptionalAnalytics to track page views with pathname and search parameters

--- components/OptionalAnalytics.tsx
+++ components/OptionalAnalytics.tsx
@@ -1,13 +1,35 @@
 'use client';
 
+import { useEffect } from 'react';
 import Script from 'next/script';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 const fallbackAnalyticsId = 'G-LZTLB12GK5';
 const analyticsId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || fallbackAnalyticsId;
 
 const shouldRender = Boolean(analyticsId);
 
+function getFullPath(pathname: string | null, search: string | undefined) {
+  if (!pathname) return '/';
+  return search ? `${pathname}?${search}` : pathname;
+}
+
 export function OptionalAnalytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (!shouldRender) return;
+    const gtag = (window as typeof window & { gtag?: (...args: unknown[]) => void }).gtag;
+    if (!gtag) return;
+    const search = searchParams?.toString();
+    const pagePath = getFullPath(pathname, search);
+    gtag('config', analyticsId, {
+      page_path: pagePath,
+      page_location: window.location.href
+    });
+  }, [pathname, searchParams]);
+
   if (!shouldRender) return null;
 
   return (
@@ -18,7 +40,7 @@ export function OptionalAnalytics() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${analyticsId}');
+          gtag('config', '${analyticsId}', { send_page_view: false });
         `}
       </Script>
     </>
